3664215f8ae231fff4d042e3ecf03a5c512ad922
mandaltanu <itanumaymondal@gmail.com>
2024-11-14 16:51:14 +0530

    1st commit

---
+++ js/array.js
@@ -0,0 +1,21 @@
+// let a=[1,2,3,4,5]
+
+// console.log(a[0])
+// let [b,c,d,e,f]=a
+
+// console.log(b)
+
+let a=[1,2,3,4,5,6]
+
+// let b=a.push(11,12,13)
+// console.log(a)
+
+// let b=a.pop()
+// console.log(a)
+
+// let b=a.unshift(50)
+// console.log(a)
+
+let b=a.shift()
+console.log(a)
+
---
+++ js/var.js
@@ -0,0 +1,23 @@
+// function hello(){
+//     let a = 12
+//     console.log(a)
+// }
+// hello()
+
+
+// let a = 12
+
+// function hello(){
+//     // let a = 30
+//     console.log(a)
+//     {
+//         // let t = 45
+//         console.log(a)
+//     }
+//     console.log(t)
+// }
+// hello()
+// console.log(a)
+
+
+
