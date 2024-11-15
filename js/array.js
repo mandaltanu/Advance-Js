2b822343fde052d01362d392c83513fe5445c8ed
mandaltanu <itanumaymondal@gmail.com>
2024-11-15 18:16:13 +0530

    2nd commit

--- js/array.js
+++ js/array.js
@@ -5,17 +5,37 @@
 
 // console.log(b)
 
-let a=[1,2,3,4,5,6]
 
+// let a=[1,2,3,4,5,6]
 // let b=a.push(11,12,13)
 // console.log(a)
-
 // let b=a.pop()
 // console.log(a)
-
 // let b=a.unshift(50)
 // console.log(a)
+// let b=a.shift()
+// console.log(a)
 
-let b=a.shift()
+
+// let a =[1,2,3,4,5,6,7,8,9]
+// let b =a.slice(0,4)
+// let p=a.slice(4,8)
+// console.log(b)
+// console.log(p)
+
+
+// let a =[1,2,3,4,5,6,7,8,9]
+// let b=a.splice(5,2)
+// let p=a.splice(0,7)
+// console.log(a)
+
+
+// let a =[1,2,3,4,5,6,7,8,9]
+// let b =a.splice(5,2,"op")
+// console.log(a)
+
+
+let a =[1,2,3,4,5,6,7,8,9]
+let b =a.splice(4,0,"op")
+let c =a.splice(7,0,"io")
 console.log(a)
-
