f2f5566bdee861c3832c6844687ee3aeaf623dde
mandaltanu <itanumaymondal@gmail.com>
2024-11-20 17:15:05 +0530

    3rd commit

--- js/array.js
+++ js/array.js
@@ -30,12 +30,42 @@
 // console.log(a)
 
 
+//  let a =[1,2,3,4,5,6,7,8,9]
+//  let b =a.splice(5,1,"op")
+//  console.log(a)
+
+
 // let a =[1,2,3,4,5,6,7,8,9]
-// let b =a.splice(5,2,"op")
+// let b =a.splice(4,0,"op")
+// let c =a.splice(7,0,"io")
 // console.log(a)
 
 
-let a =[1,2,3,4,5,6,7,8,9]
-let b =a.splice(4,0,"op")
-let c =a.splice(7,0,"io")
-console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/loop.js
@@ -0,0 +1,43 @@
+// let a=[1,2,3,4,5,6,7,8,9,10]
+
+// let t=[]
+// for(let i=0;i<a.length;i++){
+//     t.push(a[i]*2)
+//     // console.log(t)
+// }
+// console.log(t)
+// for(let i=a.length-1;i>=0;i--){
+//     console.log(a[i])
+// }
+
+
+
+// for in loop: by defult index no print hoii
+
+// let a=[1,2,3,4,5,6,7,8,9,10]
+
+// let t=[]
+// for(let i in a){
+//     console.log(i)
+//     // console.log(a[i])
+// }
+
+// for of loop : return only value
+
+// let a=[1,2,3,4,5,6,7,8,9,10]
+
+// let t=[]
+// for(let i of a){
+//     console.log(i)
+// }
+
+// forEach loop
+// let a=[1,2,3,4,5,6,7,8,9,10]
+
+// let t=[]
+// a.forEach((item)=>{
+//     console.log(item)
+// })
+
+
+// skdsjnkonvfv,m 
